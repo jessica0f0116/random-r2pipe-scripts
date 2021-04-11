36db2bb8a142baed82f71f013c7a8e7b15c69dea
jessica0f0116 <72535217+jessica0f0116@users.noreply.github.com>
2021-04-11 06:16:32 -0400

    Add files via upload

---
+++ get_syscall_numbers.py
@@ -0,0 +1,22 @@
+import r2pipe
+import sys
+
+r2 = r2pipe.open()
+file = open('win32u_syscalls.json', 'w')
+
+num_syscalls = r2.cmd('aflc')
+firstline = '{{\"number of syscalls\":\"{}\", \"syscall entries\":[\r\n'.format(
+    num_syscalls.rstrip())
+file.write(firstline)
+addrs = r2.cmdj('aflqj')
+for addr in addrs:
+    hexaddr = hex(addr)
+    syscallnum = r2.cmd("p8 1 @ {}+5".format(hexaddr)).replace('\n', '')
+    syscallnum += r2.cmd("p8 1 @ {}+4".format(hexaddr)).replace('\n', '')
+    syscallname = r2.cmdj("afij @ {}".format(hexaddr))[0]['name'][15:]
+    if syscallnum == 'cccc':
+        continue
+    jsonline = '{{\"syscall name":\"{}\", \"syscall number\":\"0x{}\"}},\r\n'.format(
+        syscallname, syscallnum)
+    file.write(jsonline)
+file.write(']}')
